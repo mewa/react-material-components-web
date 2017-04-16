c69c06eada1566915abbdfbfecf7a9b70edb1879
mewa <marcin.k.chmiel@gmail.com>
2017-04-17 00:46:30 +0200

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
-const path = require("path");
-const package = require("./package.json");
+'use strict';
 
-module.exports = {
-    entry: "./src/index.js",
+const webpack = require('webpack');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const path = require("path");
+const libName = "lib";
+
+var config = {
+    target: "node",
+    entry: {
+	main: "./src/index.js"
+    },
     output: {
-	library: package.name,
+	library: libName,
+	libraryTarget: "umd",
 	path: path.resolve(__dirname, "build"),
-	filename: "[name].[hash].bundle.js"
+	filename: libName + ".js"
     },
     module: {
 	rules: [
@@ -14,5 +22,13 @@ module.exports = {
 		test: /\.(js|jsx)$/, use: "babel-loader"
 	    }
 	]
-    }
+    },
+    resolve: {
+	modules: [path.resolve(__dirname, "src"), "node_modules"]
+    },
+    plugins: [
+	new ManifestPlugin()
+    ]
 };
+
+module.exports = config;
