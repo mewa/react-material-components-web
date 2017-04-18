b4abb0ee554b2937cb0a91bb94d9b61f975ba13c
mchmiel <marcin.k.chmiel@gmail.com>
2017-04-18 13:28:45 +0200

    Added horizontal stacking and vertical actions

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import { Primary, Title, Subtitle } from "Card/CardPrimary.jsx";
-import { Action, Media, SupportingText } from "Card/CardContent.jsx";
+import { Action, Actions, Horizontal, Media, MediaItem, SupportingText } from "Card/CardContent.jsx";
 
 var base = ["mdc-card"];
-var baseActions = ["mdc-card__actions"];
 
 export default class Card extends React.Component {
     constructor(props) {
@@ -15,34 +14,12 @@ export default class Card extends React.Component {
 	this.classes = classNames(base, this.props.className, {
 	    "mdc-card--dark": this.props.dark
 	});
-
-	this.actionClasses = classNames(baseActions, {
-	    "mdc-card__actions--vertical": this.props.verticalActions
-	});
-	
-	this.actions = [];
-	this.content = [];
-	React.Children.forEach(this.props.children, (child) => {
-	    switch (child.type) {
-	    case Action:
-		    this.actions.push(child);
-		break;
-	    default:
-		    this.content.push(child);
-	    }
-	});
     }
     
     render() {
 	let { dark, verticalActions, horizontal, className, maxWidth, ...props} = this.props;
 	return (
 	    <div {...props} className={this.classes} style={{maxWidth}}>
-		{this.content}
-		{this.actions.length > 0 &&
-		    <section className={this.actionClasses} >
-		    {this.actions}
-		    </section>
-		}
 	    </div>
 	);
     }
@@ -51,6 +28,7 @@ export default class Card extends React.Component {
 Card.propTypes = {
     dark: PropTypes.bool,
     verticalActions: PropTypes.bool,
+    horizontal: PropTypes.bool,
     maxWidth: PropTypes.string
 };
 
@@ -65,3 +43,6 @@ Card.Action = Action;
 Card.Media = Media;
 Card.Content = SupportingText;
 Card.Header = Primary;
+Card.MediaItem = MediaItem;
+Card.Horizontal = Horizontal;
+Card.Actions = Actions;
--- src/Card/CardContent.jsx
+++ src/Card/CardContent.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+var Actions = (props) => {
+    var classes = classNames(["mdc-card__actions"], props.className, {
+	"mdc-card__actions--vertical": props.vertical
+    });
+    const { vertical, ...p } = props;
+    return <section className={classes} {...p} />;
+}
 
 var Action = (props) => {
     return <button className="mdc-button mdc-button--compact mdc-card__action" {...props}>{props.children}</button>;
@@ -10,19 +19,47 @@ var SupportingText = (props) => {
 };
 
 var Media = (props) => {
-    var height = props.height;
-    if (!height) {
-	height = 90;
-    }
     var style = {
 	background: "url(" + props.src + ")",
 	backgroundSize: "cover",
 	backgroundRepeat: "no-repeat",
-	height: height
+	height: props.height
     };
     return <section className="mdc-card__media" style={style} {...props} ></section>;
 };
 
+Media.propTypes = {
+    src: PropTypes.string.isRequired,
+    height: PropTypes.string
+};
+
+Media.defaultProps = {
+    height: "12.313rem"
+};
+
+var MediaItem = (props) => {
+    const classes = classNames(["mdc-card__media-item"], props.className, {
+	"mdc-card__media-item--1dot5x": props.x1dot5,
+	"mdc-card__media-item--2x": props.x2,
+	"mdc-card__media-item--3x": props.x3
+    })
+    return <img className={classes} src={props.src} style={{height: props.height}}/>  
+};
+
+MediaItem.propTypes = {
+    height: PropTypes.string,
+    x1dot5: PropTypes.bool,
+    x2: PropTypes.bool,
+    x3: PropTypes.bool
+};
+
+var Horizontal = (props) => {
+    return <div className="mdc-card__horizontal-block" {...props} />;
+}
+
+exports.Actions = Actions;
 exports.Action = Action;
 exports.SupportingText = SupportingText;
 exports.Media = Media;
+exports.MediaItem = MediaItem;
+exports.Horizontal = Horizontal;
