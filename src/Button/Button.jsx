7efc663aed6a46512d2bd90df673b234ed3175f9
mchmiel <marcin.k.chmiel@gmail.com>
2017-04-20 00:46:36 +0200

    Updated Button ripple to use MDCRipple Foundation

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,34 +1,101 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { MDCRipple } from '@material/ripple';
+import { MDCRipple, MDCRippleFoundation } from '@material/ripple';
+import * as util from '@material/ripple/util.js';
+import { Map } from 'immutable';
 
 export default class Button extends React.Component {
     constructor(props) {
 	super(props);
 
-	this.classes = classNames(["mdc-button"], this.props.className, {
-	    "mdc-button--accent": this.props.accent,
-	    "mdc-button--primary": this.props.primary,
-	    "mdc-button--raised": this.props.raised,
-	    "mdc-button--compact": this.props.compact,
-	    "mdc-button--dense" : this.props.dense
+	this.classes = ["mdc-button"]
+	
+	this.state = {
+	    classes: classNames(this.classes, this.props.className, {
+		"mdc-button--accent": this.props.accent,
+		"mdc-button--primary": this.props.primary,
+		"mdc-button--raised": this.props.raised,
+		"mdc-button--compact": this.props.compact,
+		"mdc-button--dense" : this.props.dense
+	    }),
+	    ripple: new Map()
+	};
+
+	var MATCHES = util.getMatchesProperty(HTMLElement.prototype);
+	this.rippleFoundation = new MDCRippleFoundation({
+	    browserSupportsCssVars: () => util.supportsCssVariables(window),
+	    addClass: (c) => this.addClass(c),
+	    removeClass: (c) => this.removeClass(c),
+	    computeBoundingRect: () => this.root.getBoundingClientRect(),
+	    isSurfaceActive: () => this.root[MATCHES](':active'),
+	    isUnbounded:  () => false,
+	    registerInteractionHandler: (evtType, handler) => this.root.addEventListener(evtType, handler),
+	    deregisterInteractionHandler: (evtType, handler) => this.root.removeEventListener(evtType, handler),
+	    registerResizeHandler: (handler) => window.addEventListener('resize', handler),
+	    deregisterResizeHandler: (handler) => window.removeEventListener('resize', handler),
+	    updateCssVariable: (name, value) => {
+		this.setState({
+		    ...this.state,
+		    ripple: this.state.ripple.set(name, value)
+		});
+	    },
+	    getWindowPageOffset: () => ({x: window.pageXOffset, y: window.pageYOffset})
 	});
-	console.log(this.classes);
+    }
+
+    addClass(className) {
+	this.classes.push(className);
+	this.setState({
+	    ...this.state,
+	    classes: classNames(this.classes, this.props.className, {
+		"mdc-button--accent": this.props.accent,
+		"mdc-button--primary": this.props.primary,
+		"mdc-button--raised": this.props.raised,
+		"mdc-button--compact": this.props.compact,
+		"mdc-button--dense" : this.props.dense
+	    })
+	});
+    }
+
+    removeClass(className) {
+	let i = this.classes.indexOf(className);
+	if (i > -1)
+	    this.classes.splice(i, 1);
+	this.setState({
+	    ...this.state,
+	    classes: classNames(this.classes, this.props.className, {
+		"mdc-button--accent": this.props.accent,
+		"mdc-button--primary": this.props.primary,
+		"mdc-button--raised": this.props.raised,
+		"mdc-button--compact": this.props.compact,
+		"mdc-button--dense" : this.props.dense
+	    })
+	});
     }
 
     render() {
 	const { accent, primary, raised, href, compact, dense, ripple, noripple, className, ...p } = this.props;
 	if (href)
-	    return <a href={href} className={this.classes} {...p} ref={ (btn) => {
-		if (!noripple && ripple)
-		    MDCRipple.attachTo(btn);
-	    }} />;
+	    return <a href={href} className={this.state.classes} {...p} />;
 	else
-	    return <button className={this.classes} {...p} ref={ (btn) => {
-		if (!noripple && ripple)
-		    MDCRipple.attachTo(btn);
-	    }} />
+	    return <button className={this.state.classes} {...p} ref={ (node) => this.root = node }/>
+    }
+
+    componentDidMount() {
+	this.rippleFoundation.init();
+    }
+
+    componentWillUnmount() {
+	this.rippleFoundation.destroy();
+    }
+
+    componentDidUpdate() {
+	if (this.root) {
+	    this.state.ripple.forEach((v, k) => {
+		this.root.style.setProperty(k, v);
+	    })
+	}
     }
 
 };
